4469bcbfbee2b2d032ac68604cabdd8689877846
Sahand Tebyani <sahadina10@gmail.com>
2022-02-11 17:07:48 +0200

    Password reset by email feature added

--- devfinder/urls.py
+++ devfinder/urls.py
@@ -17,13 +17,23 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('projects/', include('projects.urls')),
     path('', include('users.urls')),
     path('admin/', admin.site.urls),
-]
 
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name='password-reset-form.html'),
+         name='reset_password'),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name='reset-password-sent.html'),
+         name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='set-password.html'),
+         name='password_reset_confirm'),
+    path('reset_password_complete/',
+         auth_views.PasswordResetCompleteView.as_view(template_name='reset-password-done.html'),
+         name='password_reset_complete'),
+]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.STATIC_ROOT)
--- users/signals.py
+++ users/signals.py
@@ -2,6 +2,8 @@ from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from .models import Profile
 from django.contrib.auth.models import User
+from django.core.mail import send_mail
+from django.conf import settings
 
 
 @receiver(post_save, sender=User)
@@ -15,6 +17,16 @@ def create_profile(sender, instance, created, **kwargs):
             name=f'{user.first_name} {user.last_name}'
         )
 
+        subject = 'Welcome to Devsearch'
+        message = 'We are glad you are here'
+
+        send_mail(
+            subject,
+            message,
+            settings.EMAIL_HOST,
+            [profile.email],
+            fail_silently=False
+        )
 
 @receiver(post_save, sender=Profile)
 def update_user(sender, instance, created, **kwargs):
