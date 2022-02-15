1f1ac4bc578f8fe1d8c7058154df7236acd4ee64
Sahand Tebyani <sahadina10@gmail.com>
2022-02-15 20:54:49 +0200

    Json web tokens and review api added

--- api/views.py
+++ api/views.py
@@ -1,8 +1,9 @@
 from django.http import JsonResponse
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from .serializers import ProjectSerializer
-from projects.models import Project
+from projects.models import Project, Review
 
 
 @api_view(['GET'])
@@ -19,6 +20,7 @@ def get_routes(request):
 
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def get_projects(request):
     projects = Project.objects.all()
     serializer = ProjectSerializer(projects, many=True)
@@ -31,4 +33,24 @@ def get_project(request, pk):
     project = Project.objects.get(id=pk)
     serializer = ProjectSerializer(project, many=False)
 
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def project_vote(request, pk):
+    project = Project.objects.get(id=pk)
+    user = request.user.profile
+    data = request.data
+
+    review, created = Review.objects.get_or_create(
+        owner=user,
+        project=project
+    )
+
+    review.vote = data
+    review.save()
+    project.get_vote_count
+
+    serializer = ProjectSerializer(project, many=False)
     return Response(serializer.data)
