33aa38e6fe9d4e2c406a75b9ad8eb17a3e45d1eb
Sahand Tebyani <sahadina10@gmail.com>
2022-02-20 12:26:01 +0200

    Anonymes user message was fixed

--- projects/models.py
+++ projects/models.py
@@ -5,7 +5,7 @@ from users.models import Profile
 
 
 class Project(models.Model):
-    owner = models.ForeignKey(Profile, null=True, blank=True, on_delete=models.SET_NULL)
+    owner = models.ForeignKey(Profile, null=True, blank=True, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     featured_image = models.ImageField(null=True, blank=True, default='default.jpg')
